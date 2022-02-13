c432f1bcaaf0d9af1b30765eb62e2aa5a46d3819
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2022-02-14 06:51:14 +0800

    update colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import {ThemeProvider} from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <ThemeProvider enableColorScheme>
       <Layout title={pageProps.title}>
         <Component {...pageProps} />
       </Layout>
