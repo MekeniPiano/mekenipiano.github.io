04d69cfc3206fd9f7522ae5020704ee2c6f05cd5
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2022-02-14 04:39:51 +0800

    test blog

---
+++ pages/blog/first-post.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Post: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>First post</title>
+      </Head>
+      My first post
+    </>
+  );
+};
+
+export default Post;
---
+++ pages/blog/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Blog: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      Blog
+    </>
+  );
+};
+
+export default Blog;
